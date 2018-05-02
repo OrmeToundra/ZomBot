a0cc774214b151a24e9d8c25ceea28da9c1f6c33
OrmeToundra <38925337+OrmeToundra@users.noreply.github.com>
2018-05-03 01:19:37 +0200

    Add files via upload

---
+++ Zombot V.1.js
@@ -0,0 +1,64 @@
+bot.on('guildMemberAdd', member => {
+  member.createDM().then(channel => {
+    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
+  }).catch(console.error)
+  
+})
+
+bot.on('message', message => {
+  if (message.content === 'yo') {
+    message.reply('Salut !, ça va ? ')
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === 'Salut') {
+    message.reply('Salut !, ça va ? ')
+  }
+})
+
+bot.on('message', message => {
+  if (message.content === 'slt') {
+    message.reply('Salut !, ça va ? ')
+  }
+})
+
+
+bot.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    
+    let args = message.content.split(' ')
+    
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+})
+
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login('NDQxMzI4NjMxNzkzNTgyMDkw.Dcu3Jw.vmADKQENbA8CEkUmOWHIzvM9djo')
